46e93f0ac78b218bb4400321f6becf06494734d4
lindazhao678 <lindazhao678@gmail.com>
2022-11-27 22:16:50 +1100

    skills update

--- client/src/services/projectData.js
+++ client/src/services/projectData.js
@@ -14,7 +14,7 @@ const data = [
     codeURL: "https://github.com/lindazhao678/Recipe_search_app",
     brief: "Recipe Search App is a react web application for user to search and view the food recipe. User can manage their favorite recipes in their favorite page. This web app provides basic search, pagination, and customization features.",
     tech: ["Build the frontend with React.", "Build the backend with Express.", "Use SASS and Bootstrap for the styling and responsivness.", "Use React Hook Form combined with Joi library to build the contact form and validation system.", "Use React Router DOM to implement dynamic routing.", "Design wireframe and prototype with Figma.", "Hoseted on Vercel."],
-    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'react', 'bootstrap']
+    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'reactjs', 'bootstrap']
   },
   {
     id: 2,
@@ -24,7 +24,7 @@ const data = [
     codeURL: "https://github.com/lindazhao678/pixel_bay",
     brief: "Picture Search App is a react web application for picture search. User can search pictures by tags. It's built with dynamic features, such as pagination, zoom in picture, and light/dark mode.",
     tech: ["Build the frontend with React.", "Build the backend with Express.", "Use CSS and Bootstrap for the styling.", "Use Axios library to fatch data.", "Use React Router DOM to implement dynamic routing.", "Hoseted on Heroku."],
-    skills: ['html', 'css', 'javascript', 'react', 'bootstrap']
+    skills: ['html', 'css', 'javascript', 'reactjs', 'bootstrap']
   },
   {
     id: 3,
@@ -34,7 +34,7 @@ const data = [
     codeURL: "https://github.com/lindazhao678/weather-app",
     brief: "Weather App is a react web appication which hosted on Heroku. This app provides Melbourne weather forecasts with some friendly reminders based on the weather. Reminders include apply sunscream, wear jacket or Bring umbrella. For example, if the UV is higher than certain number, the app will remind the user to apply suncream.",
     tech: ["Build the frontend with React.", "Build the backend with Express.", "Use React Bootstrap for the styling.", "Use Axios library to fatch data.", "Use React Router DOM to implement dynamic routing.", "Hoseted on Heroku."],
-    skills: ['html', 'css', 'javascript', 'react', 'bootstrap']
+    skills: ['html', 'css', 'javascript', 'reactjs', 'bootstrap']
   },
   {
     id: 4,
@@ -44,7 +44,7 @@ const data = [
     codeURL: "https://github.com/lindazhao678/Portfolio_site",
     brief: "My portfolio website was built with react and hosted on Netlify. It's responsive through the use of bootstrap and media query. User can check the summary of each of my projects on the project page, and can also opt to dive into the code or live site with the external link. Skills are linked with the corresponding projects. Page scrolling was implemented on the home page.",
     tech: ["Build the frontend with React.", "Use SASS for the styling.", "Use carousel library to display animate pictures for the project.", "Use React Router DOM to implement dynamic routing.", "Design moodboard with Milanote.", "Design wireframe and prototype with Figma.", "Hoseted on Netlify."],
-    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'milanote', 'react', 'bootstrap', 'photoshop']
+    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'milanote', 'reactjs', 'bootstrap', 'photoshop']
   },
   {
     id: 5,
@@ -54,7 +54,7 @@ const data = [
     codeURL: "https://github.com/lindazhao678/FAANG-tracker-app",
     brief: "FAANG Tracker is a news media application which was built with next and hosted on Vercel. It provides the live stock updates, the latest news, investment advice and resources of Facebook, Amazon, Apple, Netflix, and Google. The app design is responsive and mobile friendly. The goal of this web application is to help anyone who are interested or who have already invested at the five FAANG stocks to be timely advised and be able to make the right investment decisions.",
     tech: ["Build the front-end with React.", "Build the server-side with next.", "Use SASS for the styling.", "Use pre-rendering methodology for fetching the data from the external API.", "Integrate Google Analytics into the deployed website.", "Design wireframe and prototype with Figma.","Hoseted on Vercel."],
-    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'nextjs', 'react']
+    skills: ['html', 'css', 'sass', 'javascript', 'figma', 'nextjs', 'reactjs']
   },
   {
     id: 6,
--- client/src/services/skillData.js
+++ client/src/services/skillData.js
@@ -1,94 +1,124 @@
 const data = [
     {
         id: 1,
-        name: "HTML",
-        handleSkillName: "html"
-    },
-    {
-        id: 2,
         name: "JavaScript",
         handleSkillName: "javascript"
     },
     {
-        id: 3,
+        id: 2,
         name: "TypeScript",
         handleSkillName: "typescript"
     },
     {
-        id: 4,
+        id: 3,
         name: "ReactJS",
-        handleSkillName: "react"
+        handleSkillName: "reactjs"
     },
     {
-        id: 5,
-        name: "CSS/SASS",
-        handleSkillName: "sass"
-    },
-    {
-        id: 6,
-        name: "Bootstrap",
-        handleSkillName: "bootstrap"
-    },
-    {
-        id: 7,
-        name: "TailwindCSS",
-        handleSkillName: "tailwindcss"
-    },
-    {
-        id: 8,
-        name: "Figma",
-        handleSkillName: "figma"
-    },
-    {
-        id: 9,
-        name: "Milanote",
-        handleSkillName: "milanote"
-    },
-    {
-        id: 10,
-        name: "Photoshop",
-        handleSkillName: "photoshop"
-    },
-    {
-        id: 11,
-        name: "NodeJS",
-        handleSkillName: "nodejs"
-    },
-    {
-        id: 12,
-        name: "Express",
-        handleSkillName: "express"
-    },
-    {
-        id: 13,
-        name: "MongoDB",
-        handleSkillName: "mongodb"
-    },
-    {
-        id: 14,
+        id: 4,
         name: "NextJS",
         handleSkillName: "nextjs"
     },
     {
-        id: 15,
+        id: 5,
+        name: "Svelte",
+        handleSkillName: "svelte"
+    },
+    {
+        id: 6,
+        name: "HTML",
+        handleSkillName: "html"
+    },
+    {
+        id: 7,
+        name: "CSS/SASS",
+        handleSkillName: "sass"
+    },
+    {
+        id: 8,
+        name: "Bootstrap",
+        handleSkillName: "bootstrap"
+    },
+    {
+        id: 9,
+        name: "MUI",
+        handleSkillName: "mui"
+    },
+    {
+        id: 10,
+        name: "NodeJS",
+        handleSkillName: "nodejs"
+    },
+    {
+        id: 11,
+        name: "Express",
+        handleSkillName: "express"
+    },   
+    {
+        id: 12,
         name: "Restful API",
         handleSkillName: "restfulapi"
     },
     {
-        id: 16,
+        id: 13,
         name: "GraphQL",
         handleSkillName: "graphql"
     },
     {
-        id: 17,
+        id: 14,
         name: "MySQL",
         handleSkillName: "mysql"
     },
     {
+        id: 15,
+        name: "MongoDB",
+        handleSkillName: "mongodb"
+    },
+    {
+        id: 16,
+        name: "Firebase",
+        handleSkillName: "firebase"
+    },
+    {
+        id: 17,
+        name: "Postman",
+        handleSkillName: "postman"
+    }, 
+    {
         id: 18,
-        name: "Svelte",
-        handleSkillName: "svelte"
-    }
+        name: "Figma",
+        handleSkillName: "figma"
+    },
+    {
+        id: 19,
+        name: "Milanote",
+        handleSkillName: "milanote"
+    },
+    {
+        id: 20,
+        name: "Photoshop",
+        handleSkillName: "photoshop"
+    },
+    {
+        id: 21,
+        name: "Git",
+        handleSkillName: "git"
+    },
+    {
+        id: 22,
+        name: "Trello",
+        handleSkillName: "trello"
+    },
+    {
+        id: 23,
+        name: "GCP",
+        handleSkillName: "gcp"
+    },
+    {
+        id: 24,
+        name: "Docker",
+        handleSkillName: "docker"
+    },
 
 ];
 
