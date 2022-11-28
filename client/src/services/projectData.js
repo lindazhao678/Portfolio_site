44e47ed2dfcc87789a5796f8834d672fc972eb20
lindazhao678 <lindazhao678@gmail.com>
2022-11-29 00:03:11 +1100

    project data update

--- client/src/services/projectData.js
+++ client/src/services/projectData.js
@@ -71,7 +71,7 @@ const data = [
     id: 7,
     name: "Movies World App",
     img: projectImg7,
-    siteURL: "https://github.com/lindazhao678/movies-world-app",
+    siteURL: "https://movies-world-client.vercel.app/",
     codeURL: "https://github.com/lindazhao678/movies-world-app",
     brief: "An online movies world DVDs shop built using React as the frontend, node and express as the backend, and Google Firestore as the database. Users can visit the movie gallery, which will be  retrieved from the external API. User can look up all our DVDs in store, the details of our DVDs, and search our DVDs in store. Users can also sign up and login. Admin user with login can add DVDs, edit DVDs, and delete DVDs.",
     tech: ["Build the frontend with React.", "Build the backend with Express.", "Build the APIs with Restful API.", "Store the data in Firebase database.", "Store the image files in firebase storage bucket.", "Use Joi labrary for validation.", "Use Json Web Token for authentication.", "Use bcrypt library for hashing and salting passwords.", "Use Typescript to constrain the data types."],
